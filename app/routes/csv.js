9fcd27492a8daa73c1ce5206361d5cfd174bffd0
markon1 <zmstefan@gmail.com>
2019-04-15 19:04:52 +0200

    reverted to csv response instead of file generation

--- app/routes/csv.js
+++ app/routes/csv.js
@@ -1,6 +1,4 @@
 var mongoose = require("mongoose");
-const fs = require("fs");
-const path = require("path");
 const Json2csvParser = require("json2csv").Parser;
 var Follow = require("../models/follow");
 const fields = ["brand", "handle", "title", "followDateTime", "posts", "followers", "following", "description", "link", "isPrivate"];
@@ -15,26 +13,9 @@ module.exports = function(app) {
 				if (err) {
 					res.status(500).send(err);
 				} else {
-					let csv;
-					try {
-						const json2csvParser = new Json2csvParser({ fields });
-						csv = json2csvParser.parse(follows);
-					} catch (err) {
-						return res.status(500).json({ err });
-					}
-					const dateTime = Date.now();
-					const filePath = path.join(__dirname, "..", "public", "exports", "csv-" + dateTime + ".csv");
-
-					fs.writeFile(filePath, csv, function(err) {
-						if (err) {
-							return res.json(err).status(500);
-						} else {
-							setTimeout(function() {
-								fs.unlinkSync(filePath); // delete this file after 30 seconds
-							}, 30000);
-							return res.json("/exports/csv-" + dateTime + ".csv");
-						}
-					});
+					res.attachment("stats.csv");
+					const json2csvParser = new Json2csvParser({ fields });
+					res.status(200).send(json2csvParser.parse(follows));
 				}
 			});
 		}
