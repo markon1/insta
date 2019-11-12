56214fd012765208642964dd2a07401365019882
markon1 <zmstefan@gmail.com>
2019-11-12 21:26:02 +0100

    { useUnifiedTopology: true }

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 // set the port of app
 var port = process.env.PORT || 8080;
 // connect to our database
-mongoose.connect(configDB.url);
+mongoose.connect(configDB.url,{ useUnifiedTopology: true });
 
 // log every request to the console
 app.use(morgan("dev"));
